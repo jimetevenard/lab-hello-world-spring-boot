f6b31d9fa52abc61caa2d5a34e864ac9eda0b77d
Jim Etevenard <jimetevenard@gmail.com>
2022-10-18 12:18:31 +0200

    Update : add comment

--- src/main/java/com/jimetevenard/helloworldspringboot/CapitalizeService.java
+++ src/main/java/com/jimetevenard/helloworldspringboot/CapitalizeService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class CapitalizeService {
 
+    /**
+     * @return The given input, capitalized.
+     */
     public String capitalize(String input){
         return input.toUpperCase();
     }
